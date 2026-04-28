9385bf8d88038a5a69e0f7a38a85c3ff67edea93
variannn340 <varianvarian0301@gmail.com>
2026-04-28 09:36:10 +0700

    Full class Mahasiswa

---
+++ classMahasiswa.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa 
+{ // Akses Modifier
+    int NIM;
+    string nama;
+    float nilai;
+
+    void pirntdata()
+    {
+        cout << "NIM = " << NIM << endl;
+        cout << "Nama = " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
+}
+
+
