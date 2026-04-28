f2900801771391ff8dc6b900b6f7ca535a3436c1
variannn340 <varianvarian0301@gmail.com>
2026-04-28 11:27:05 +0700

    declare void tampilkan()

--- dataBarang.cpp
+++ dataBarang.cpp
@@ -2,9 +2,20 @@
 using namespace std;
 
 
-class Barang {
-public:
+class Barang 
+{
+    public:
     string nama;
     int    jumlah;
     string kategori;
     string tanggalProduksi;
+
+    void tampilkan()
+    {
+        cout << "------------------------" << endl;
+        cout << "Nama            : " << nama            << endl;
+        cout << "Jumlah          : " << jumlah          << endl;
+        cout << "Kategori        : " << kategori        << endl;
+        cout << "Tanggal Produksi: " << tanggalProduksi << endl;
+        cout << "------------------------" << endl;
+    }
